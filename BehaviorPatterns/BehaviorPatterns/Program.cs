e2d4c0120dc38ffbd6a181a4ffdee73407aaec25
PressFtoPayTaxes <46023537+PressFtoPayTaxes@users.noreply.github.com>
2019-07-07 00:04:12 +0600

    Init

---
+++ BehaviorPatterns/BehaviorPatterns/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility
+{
+    interface IHandler
+    {
+        IHandler SetNext(IHandler handler);
+        void Handle(string request);
+    }
+
+    abstract class RegistrationHandler : IHandler // Base handler
+    {
+        private IHandler _next;
+
+        public virtual void Handle(string request)
+        {
+            if (_next != null)
+                _next.Handle(request);
+            else
+                Console.WriteLine("Unknown request");
+        }
+
+        public IHandler SetNext(IHandler handler)
+        {
+            _next = handler;
+            return handler;
+        }
+    }
+
+    class PasswordChecker : RegistrationHandler // Concrete handler
+    {
+        public override void Handle(string request)
+        {
+            if (request == "Check password")
+                Console.WriteLine("Password checker: Password checked");
+            else
+                base.Handle(request);
+        }
+    }
+
+    class DatabaseWriter : RegistrationHandler // Concrete handler
+    {
+        public override void Handle(string request)
+        {
+            if (request == "Save to database")
+                Console.WriteLine("Database writer: Data saved to database");
+            else
+                base.Handle(request);
+        }
+    }
+
+    class Verifier : RegistrationHandler // Concrete handler
+    {
+        public override void Handle(string request)
+        {
+            if (request == "Verify")
+                Console.WriteLine("Verifier: User verified");
+            else
+                base.Handle(request);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegistrationHandler passwordChecker = new PasswordChecker();
+            RegistrationHandler databaseWriter = new DatabaseWriter();
+            RegistrationHandler verifier = new Verifier();
+
+            passwordChecker.SetNext(databaseWriter).SetNext(verifier);
+            Console.WriteLine("Password checker > Database writer > Verifier");
+
+            Console.WriteLine("Check password");
+            passwordChecker.Handle("Check password");
+
+            Console.WriteLine("Verify");
+            passwordChecker.Handle("Verify");
+
+            Console.WriteLine("Make coffee");
+            passwordChecker.Handle("Make coffee");
+
+            Console.WriteLine("Save to database");
+            passwordChecker.Handle("Save to database");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BehaviorPatterns/Command/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    interface ICommand
+    {
+        void Execute();
+    }
+
+    class EnterUsername : ICommand // Concrete command
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Command: Username entered");
+        }
+    }
+
+    class SaveToDatabase : ICommand // Concrete command
+    {
+        private Database _database;
+        private string _address;
+
+        public SaveToDatabase(Database database, string address)
+        {
+            _database = database;
+            _address = address;
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine("Command: Connecting to database...");
+            _database.Save(_address);
+        }
+    }
+
+    class Database // Receiver
+    {
+        public void Save(string address)
+        {
+            Console.WriteLine($"Database: Saved to database on address {address}");
+        }
+    }
+
+    class User // Invoker
+    {
+        private ICommand _command;
+
+        public void SetCommand(ICommand command)
+        {
+            _command = command;
+        }
+
+        public void ExecuteCommand()
+        {
+            if (_command != null)
+                _command.Execute();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EnterUsername enterUsername = new EnterUsername();
+            SaveToDatabase save = new SaveToDatabase(new Database(), "this_computer");
+
+            User user = new User();
+            user.SetCommand(enterUsername);
+            user.ExecuteCommand();
+            user.SetCommand(save);
+            user.ExecuteCommand();
+
+            Console.ReadLine();
+        }
+    }
+}
