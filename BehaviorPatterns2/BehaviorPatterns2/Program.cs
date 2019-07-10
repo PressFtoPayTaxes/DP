aed39035a16670f380ca2082a8afdd5a7a3ebacb
PressFtoPayTaxes <46023537+PressFtoPayTaxes@users.noreply.github.com>
2019-07-10 23:35:55 +0600

    Init

---
+++ BehaviorPatterns2/BehaviorPatterns2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    interface IListener // Observer
+    {
+        void Listen(RadioStation radioStation);
+    }
+
+    class RadioStation // Publisher
+    {
+        private List<IListener> _listeners = new List<IListener>();
+        public string PlayingRecord { get; private set; } = "Red Hot Chili Peppers - Hump de Bump";
+
+        public void ConnectListener(IListener listener) => _listeners.Add(listener);
+
+        public void DisconnectListener(IListener listener) => _listeners.Remove(listener);
+
+        public void SetNewRecord(string record)
+        {
+            PlayingRecord = record;
+            NotifyListeners();
+        }
+
+        private void NotifyListeners()
+        {
+            foreach (var listener in _listeners)
+            {
+                listener.Listen(this);
+            }
+        }
+    }
+
+    class RadioListener : IListener // ConcreteObserver
+    {
+        public void Listen(RadioStation radioStation)
+        {
+            Console.WriteLine($"RadioListener: Listening \"{radioStation.PlayingRecord}\"");
+        }
+    }
+
+    class TelephoneListener : IListener // ConcreteObserver
+    {
+        public void Listen(RadioStation radioStation)
+        {
+            Console.WriteLine($"TelephoneListener: Listening \"{radioStation.PlayingRecord}\"");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RadioListener radioListener = new RadioListener();
+            TelephoneListener telephoneListener = new TelephoneListener();
+
+            RadioStation station = new RadioStation();
+            station.ConnectListener(radioListener);
+            station.ConnectListener(telephoneListener);
+
+            Console.WriteLine("Setting new record...");
+            station.SetNewRecord("Queen - Another One Bites The Dust");
+        }
+    }
+}
---
+++ BehaviorPatterns2/TemplateMethod/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod
+{
+    abstract class Weapon
+    {
+        public void UseWeapon() // Template method
+        {
+            TakeInHand();
+            OpenStore();
+            ChargeWeapon();
+            Shoot();
+        }
+
+        public virtual void TakeInHand()
+        {
+            Console.WriteLine("Weapon is taken in hand");
+        }
+        
+        public virtual void OpenStore()
+        {
+            Console.WriteLine("The store is opened");
+        }
+
+        public virtual void ChargeWeapon()
+        {
+            Console.WriteLine("Weapon charged with bullets");
+        }
+
+        public virtual void Shoot()
+        {
+            Console.WriteLine("Shooting");
+        }
+    }
+
+    class Pistol : Weapon
+    {
+        public override void ChargeWeapon()
+        {
+            Console.WriteLine("Weapon charged with pistol bullets");
+        }
+    }
+
+    class Shotgun : Weapon
+    {
+        public override void ChargeWeapon()
+        {
+            Console.WriteLine("Weapon charged with shotgun bullets");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pistol pistol = new Pistol();
+            Console.WriteLine("Shooting with pistol");
+            pistol.UseWeapon();
+
+            Console.WriteLine();
+
+            Shotgun shotgun = new Shotgun();
+            Console.WriteLine("Shooting with shotgun");
+            shotgun.UseWeapon();
+        }
+    }
+}
