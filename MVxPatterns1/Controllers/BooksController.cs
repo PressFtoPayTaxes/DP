39078c5d5fb03290a2179c66b5117cd0e778689c
PressFtoPayTaxes <46023537+PressFtoPayTaxes@users.noreply.github.com>
2019-07-11 19:06:06 +0600

    Init

---
+++ MVxPatterns1/Controllers/BooksController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVxPatterns1.Models;
+
+namespace MVxPatterns1.Controllers
+{
+    public class BooksController : Controller
+    {
+        private BooksContext _context = new BooksContext();
+
+        public IActionResult Index()
+        {
+            IEnumerable<Book> books = _context.Books;
+            ViewBag.Books = books;
+            return View();
+        }
+
+        public IActionResult Read()
+        {
+            IEnumerable<Book> books = _context.Books;
+            ViewBag.Books = books;
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult OlderThanTwoThousand()
+        {
+            IEnumerable<Book> books = _context.Books.Where(book => book.ReleaseDate.Year < 2000);
+            ViewBag.Books = books;
+            return View();
+        }
+
+    }
+}
---
+++ MVxPatterns1/Models/BooksContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVxPatterns1.Models
+{
+    public class BooksContext : DbContext
+    {
+        public BooksContext() : base(@"Data Source=(LocalDb)\MSSQLLocalDb;AttachDbFilename='Bookstore.mdf';Integrated Security=True")
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
